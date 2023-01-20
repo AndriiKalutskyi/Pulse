fdb9fb51d2bbb061808d0449b627187adcb96cac
Andrii <andreikolyckii@gmail.com>
2023-01-20 13:54:19 +0200

    add catalog, tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// SLIDER
 $(document).ready(function () {
 	$('.slider-block__body').slick({
 		speed: 1200,
@@ -23,4 +24,39 @@ $(document).ready(function () {
 			},
 		]
 	});
+	// ТАБИ
+	$('ul.catalog-block__tabs').on('click', 'li:not(.catalog-block__tab--active)', function () {
+		$(this)
+			.addClass('catalog-block__tab--active').siblings().removeClass('catalog-block__tab--active')
+			.closest('div.catalog-block__inner').find('div.catalog-block__content').removeClass('catalog-block__content--active').eq($(this).index()).addClass('catalog-block__content--active');
+	});
+
+	// ПЕРЕХІД Детально
+	// $('.item-catalog-block__link').each(function (i) {
+	// 	$(this).on('click', function (e) {
+	// 		e.preventDefault();
+	// 		$('.item-catalog-block__content').eq(i).toggleClass('item-catalog-block__content--active');
+	// 		$('.item-catalog-block__list').eq(i).toggleClass('item-catalog-block__list--active');
+	// 	})
+	// })
+	// $('.item-catalog-block__back').each(function (i) {
+	// 	$(this).on('click', function (e) {
+	// 		e.preventDefault();
+	// 		$('.item-catalog-block__content').eq(i).toggleClass('item-catalog-block__content--active');
+	// 		$('.item-catalog-block__list').eq(i).toggleClass('item-catalog-block__list--active');
+	// 	})
+	// })
+
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.item-catalog-block__content').eq(i).toggleClass('item-catalog-block__content--active');
+				$('.item-catalog-block__list').eq(i).toggleClass('item-catalog-block__list--active');
+			});
+		});
+	};
+	toggleSlide('.item-catalog-block__link');
+	toggleSlide('.item-catalog-block__back');
+
 });
